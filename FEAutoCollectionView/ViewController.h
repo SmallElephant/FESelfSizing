e49ea9f0f251fd69829f987c03e59aa55ef0e0cb
FlyElephant <keso_xiang@talkmate.com>
2016-04-29 14:42:41 +0800

    Initial Commit

---
+++ FEAutoCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FEAutoCollectionView
+//
+//  Created by FlyElephant on 16/4/29.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FEAutoCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FEAutoCollectionView
+//
+//  Created by FlyElephant on 16/4/29.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
